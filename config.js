c631f9527107bdbda1bb035b5ba789eee737a9bf
rs4 <rayanselmi100@gmail.com>
2025-11-09 21:10:39 +0100

    fix: update DAY_END_HOUR to correct night theme start time

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 export default {
   // Time configuration (24-hour format)
   DAY_START_HOUR: 7,      // Day theme starts at 07:00
-  DAY_END_HOUR: 23,       // Night theme starts at 19:00
+  DAY_END_HOUR: 19,       // Night theme starts at 19:00
   
   // Avatar images
   AVATAR_DAY: 'avatar_day.png',
